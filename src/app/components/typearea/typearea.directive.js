11e019b96988371a42e385bfba6a54a65b95476e
Pedro Germani <pedro.germani@gmail.com>
2016-04-29 15:44:55 +0200

    using ng-repeat for the words

--- src/app/components/typearea/typearea.directive.js
+++ src/app/components/typearea/typearea.directive.js
@@ -21,19 +21,53 @@
     return directive;
 
     /** @ngInject */
-    function TypeAreaController() {
+    function TypeAreaController($scope, $element, $log) {
       var ta = this;
-      ta.originText = "original text experiment";
-      var words;
+      ta.insertedText = '';
+      
+      ta.originText = $element.attr('words');
+      $scope.words = $element.attr('words').split(' ');
+      //var words = $scope.words;
+  //    $scope.getElementById('0').toggleClass('highlight');
 
-      words = ta.originText.replace(/\w+/g, "<span>$&</span>");
-      words = words.split(" ");
-      words[0] = words[0].replace("<span>", "<span class='highlight'>");
-      ta.compareText = '';
-      for (var i=0; i < words.length; i++){
-        ta.compareText += words[i] + " ";
-      }
-      ta.words = words.toString();
+      $scope.highlight = function(event) {
+        $log.log(event.target);
+        angular.element(event.target).toggleClass('highlight');
+      };
+
+      var i=0;
+      $scope.typing = function(text) {
+        if(text == $scope.words[i]){
+            ta.insertedText += text +" ";
+            i++;
+            ta.compareText = '';
+        }
+        $log.log(text);
+
+      };
+      // var wordsArray = ta.originText.split (" ");
+      
+      // ta.compareText = '';
+      // ta.words = '';
+      
+      // for (var i=0; i < wordsArray.length; i++){
+      //   ta.words += wordsArray[i].replace(/\w+/g, "<span class='highlight'>$&</span> ");
+        
+
+
+      //   ta.compareText += wordsArray[i] + " ";
+      // }
+
+      // words = ta.originText.replace(/\w+/g, "<span>$&</span>");
+      // words = words.split(" ");
+      // words[0] = words[0].replace("<span>", "<span class='highlight'>");
+
+      
+      // for (var i=0; i < words.length; i++){
+      //   ta.compareText += words[i] + " ";
+      // }
+      
+      // ta.words = words.toString();
 
 
       // "vm.creationDate" is available by directive option "bindToController: true"
