5ca25e95c97a575e9daba3b1df6a320ae5213d0d
Enrico Icardi <enrico.icardi@gmail.com>
2016-05-22 16:57:29 +0200

    - removed injection of angular-material, causing error

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('typegame', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+    .module('typegame', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'toastr']);
 
 })();
